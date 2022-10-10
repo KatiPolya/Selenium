02b826198f520b4f4f8087ad9bf594dc509c27bd
Катерина Полякова <katsiaryna.paliakova@gmail.com>
2022-10-10 18:25:36 +0200

    test ch

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,8 +21,12 @@ public class CallbackTest {
     }
 
     @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();
+    void setupTest2() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
