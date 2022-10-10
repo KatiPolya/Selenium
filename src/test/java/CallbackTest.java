53d8a04c199b72a7c64960f657cd33c0d65f30e0
Катерина Полякова <katsiaryna.paliakova@gmail.com>
2022-10-10 18:19:02 +0200

    driver manager

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,8 +15,8 @@ public class CallbackTest {
     private WebDriver driver;
 
     @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/mac/chromedriver-1");
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
