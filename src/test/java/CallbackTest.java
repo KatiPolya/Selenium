a64f56cb287d83de60b34da18d36ef7e0a12522c
Катерина Полякова <katsiaryna.paliakova@gmail.com>
2022-10-10 17:29:08 +0200

    Test

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,27 +1,44 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
 
 
-        private WebDriver driver;
+    private WebDriver driver;
 
-        @BeforeAll
-        static void setUpAll() {
-            System.setProperty("webdriver.chrome.driver","driver/mac/chromedriver.exe");
-        }
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/mac/chromedriver");
+    }
 
-        @BeforeEach
-        void setUp() {
-            driver = new ChromeDriver();
-        }
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
 
-        @AfterEach
+    @AfterEach
     void tearDown() {
-            driver.quit();
-            driver = null;
-        }
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void test() {
+        driver.get("http://0.0.0.0:9999");
+        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Екатерина");
+        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+12345678910");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__content")).click();
+        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+
 }
